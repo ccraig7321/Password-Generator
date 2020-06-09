514e8bf6d107369b958d5f3adcd175014eec723f
Chelsey Craig <chelsey.craig7321@gmail.com>
2020-06-08 21:12:45 -0400

    Updated readme and javascript file

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
 # Password-Generator
+
+
+Generate a password with a simple click of a button!
+
+![Image of Password Generator](images/PasswordGenerator.png)
+
+___
+
+## Description
+
+Using a series of prompts, the user will be able to interact with the password generator to create a desired password.  This will combine together letters, numbers, and special characters along with a user selected character length to create a string of charters to generate a password for the user.
+___
+
+## How It Works
+
+The password generator is designed to assist the user with comprising a password that is composed with the parameters of their choosing.  The user begins the process by selecting the "Generate Password" button in the middle of the screen.
+
+![Image of Generate Button](images/GenerateButton.png)
+
+This selection will trigger the "generatePassword" function which will then give the user a series of alerts that will allow the user to decide the parameters for his/her password. An example of one of these prompts can be see below:
+
+![IMAGE of ALERT EXAMPLE](images/UppercaseLetters.png)
+
+The user has the choice of a password containing uppercase letters, lowercase letters, numbers, and special characters. These selections are then stored in an "allCharacters" array that be stored until a password length is set.
+
+From there, the user will prompted to enter a password length. The password is required to have length between 8 to 128 characters. If the user types in a number that is outside of this range, they will be met with the following alert and prompted to restart the process from the beginning:
+
+![IMAGE of PASSWORD LENGTH](CharacterCount.png)
+
+If the user selects a proper number for the length of the password, (between 8 to 128 characters), the "finalPassword" function will run and return a password that includes the parameters of the users choosing.
+
+![IMAGE of FINAL PASSWORD](images/GeneratedPasswordFINAL.png)
+
+The user will then need to hit the "Generate Password" button to restart the process again.
+
+___
+
+## Technologies
+
+- HTML
+- CSS
+- Javascript
+- Visual Studio Code
+
+___
+
+### License
+
+Copyright 2020 CHELSEY CRAIG
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
--- script.js
+++ script.js
@@ -6,7 +6,7 @@ generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 // This creates the password that the user sees after prompts
-//README: If user inputs character count within 8 - 128 charcters, function runs
+// If user inputs character count within 8 - 128 charcters, function runs
 function writePassword() {
   var password = generatePassword();
   if (password !== null) {
@@ -79,98 +79,10 @@ var upperLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 
 var lowerLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 var specialChar = ['@', '%', '+', ',', '/', '"', '!', '#', '^', '?', ':', ';', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.']
 
-// 
+// Sets low and high value
 function rand(low, high) {
+  // Defines diff variable
   var diff = high - low
+  // Returns answer from function
   return Math.floor((diff) * Math.random() + low)
 }
-
-// // Math.Random IS NEEDED FOR THIS!!
-//   return generatePassword
-// }
-
-
-//GOAL: Generate a password that contains upper case letters, lower case numbers, and/or special characters.
-// Ask if Manny want upper case letters, lower case letters, numbers, and/or special characters.
-
-
-// Do alerts to ask if variable is wanted (there would be 4 alerts for this section)  
-// Needs to be between 8 - 128 characters
-// Ask the use how long do you want the password to be
-// Create alert if under 8 charaters
-// Create alert if over 129 character
-// Answers need to be evaluated and placed into variables
-// Manny's answers will become part of the conditional (ie:
-
-
-// // var letters = ['A', 'B', 'C'];
-// // var random = Math.floor((letters.length) * Math.random())
-// //   console.log(letters[random])
-
-// var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-// var randomNumber = Math.floor((numbers.length) * Math.random())
-// console.log(numbers[randomNumber])
-
-// var numberGenerated = 0
-// for (i = 0; i < 16; i++) {
-//   var randomNumber = Math.floor((numbers.length) * Math.random())
-//   console.log(numbers[randomNumber])
-
-//   numberGenerated = numberGenerated.toString() + numbers[randomNumber].toString()
-// }
-// console.log(numberGenerated)
-
-// ///////////////////////////////////////////////////////////
-
-// var upperLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-// var randomLetterUpper = Math.floor((upperLetters.length) * Math.random())
-// console.log(upperLetters[randomUpperLetter])
-
-// var upperLetterGenerated = 0
-// for (i = 0; i < 26; i++) {
-//   randomLetterUpper = Math.floor((upperLetters.length) * Math.random())
-//   console.log(upperLetters[randomUpperLetter])
-
-//   upperLetterGenerated = upperLetterGenerated + upperLetters[randomLetterUpper]
-// }
-// console.log(upperLetterGenerated)
-
-// //////////////////////////////////////////////////////////////
-
-// var lowerLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-// var randomLetterLower = Math.floor((lowerLetter.length) * Math.random())
-// console.log(lowerLetters[randomLetterLower])
-
-// var lowerLetterGenerated = 0
-// for (i = 0; i < 26; i++) {
-//   randomLetterLower = Math.floor((lowerLetters) * Math.random())
-
-//   console.log(lowerLetters[randomLetterLower])
-
-//   lowerLetterGenerated = lowerLetterGenerated + lowerLetters[randomLetterLower]
-// }
-// console.log(lowerLetterGenerated)
-
-// //////////////////////////////////////////////////////////////
-
-// var specialChar = ['@', '%', '+', ',', '/', '"', '!', '#', '^', '?', ':', ';', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.']
-// var randomSpecialChar = Math.floor((specialChar.length) * Math.random())
-// console.log(specialChar[randomSpecialChar])
-
-// var specialCharGenerated = 0
-// for (i = 0; i < 23; i++) {
-//   randomSpecialChar = Math.floor((specialChar.length) * Math.random())
-//   console.log(specialChar[randomSpecialChar])
-
-//   specialCharGenerated = specialCharGenerated + specialChar[randomSpecialChar]
-// }
-// console.log(specialChar[randomSpecialChar])
-
-/////////////////////////////////////////////////////////////
-
-// MUST HAVE THESE TO CREATE FUNCTIONING PROGRAM!!
-// variables, conditionals, loop, function
-// FUNCTION = What is actually happening with the program (Do you want upper case letters?)
-// VARIABLE = The response/answer the function that is stored for later reference (Manny wants upper case letters)
-// CONDITIONALS = Condition of the variable (If Manny says yes, do this; if Manny says no, do something else)
-// LOOP = The repeated instructions for the program (Function loops until character count is met)
